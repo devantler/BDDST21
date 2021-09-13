c83b9776c56cb4b3de193236933efe3483475ffd
Nikolai Damm <nikolaiemildamm@icloud.com>
2021-09-13 20:10:24 +0000

    L2 exercise

--- Lecture2/parquet-word-count/example.py
+++ Lecture2/parquet-word-count/example.py
@@ -1,5 +1,8 @@
 from hdfs import InsecureClient
 from collections import Counter
+import pyarrow.parquet as pq
+import pandas as pd
+import pyarrow as pa
 
 client = InsecureClient('http://namenode:9870', user='root')
 
@@ -9,5 +12,10 @@ wordcount = None
 with client.read('/alice.txt', encoding='utf-8') as reader:
     wordcount = Counter(reader.read().split()).most_common(10)
 
-    
+
 # To-Do: Save the wordcount in a Parquet file and read it again!
+df = pd.DataFrame(wordcount)
+table = pa.Table.from_pandas(df)
+pq.write_table(table, 'example.parquet')
+table2 = pq.read_table('example.parquet')
+print(table2.to_pandas())
