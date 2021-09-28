9862707ee39a181437d0132e94263ce6fd96168d
Nikolai Damm <nikolaiemildamm@icloud.com>
2021-09-28 10:30:13 +0000

    Added python producer to lecture3

--- Lecture3/producer/example.py
+++ Lecture3/producer/example.py
@@ -1,1 +1,6 @@
-# TODO: Create a Kafka producer in Python!
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
+for _ in range(5):
+    producer.send('foo', key=b'foo', value=b'bar')
+producer.flush()
